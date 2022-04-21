4a68edc0c2f984f33aeed3216ff8e6182ef3a058
Lucas Rian <lucasrianf@hotmail.com>
2022-04-21 09:59:49 -0300

    orientado o codigo em funcoes

--- main.py
+++ main.py
@@ -1,57 +1,84 @@
-iniciar = True
+# -*- coding: utf-8 -*-
 manifestacoes = []
-id = len(manifestacoes) + 1
+id = str(len(manifestacoes) + 1)
 
 
-while(iniciar):
-    print("-----------------------------------------------")
-    print("Bem vindo ao sistema de ouvidoria da Facisa !")
-    print("-----------------------------------------------")
-    print("1 - Listar todas as manifestações")
-    print("2 - Listar todas as sugestões")
-    print("3 - Listar todas as reclamações")
-    print("4 - Listar todos os elogios")
-    print("5 - Criar uma nova manifestação")
-    print("6 - Pesquisar uma manifestação")
-    print("7 - Sair")
-    print("----------------------------------------------")
-    acao = int(input("Selecione a ação que deseja realizar: "))
+def menu():
 
-    if(acao < 1 or acao >  7):
-        print("-------------------------------------")
-        print("|         ! Ação invalida !         |")
-        continue
+   print("-----------------------------------------------")
+   print("Bem vindo ao sistema de ouvidoria da Facisa !")
+   print("-----------------------------------------------")
+   print("1 - Listar todas as manifestações")
+   print("2 - Listar todas as sugestões")
+   print("3 - Listar todas as reclamações")
+   print("4 - Listar todos os elogios")
+   print("5 - Criar uma nova manifestação")
+   print("6 - Pesquisar uma manifestação")
+   print("7 - Sair")
+   print("----------------------------------------------")
+   acao = int(input("Selecione a ação que deseja realizar: "))
+   acoes(acao)
 
-    #if(acao):
+def acoes(acaoStr):
+ acao = int(acaoStr)
+ if (acao < 1 or acao > 7):
+   print("-------------------------------------")
+   print("|         ! Ação invalida !         |")
+   menu()
 
-    #if(acao):
+ if(acao == 1):
+   listManifestacoes()
 
-    #if(acao):
+   # if(acao):
 
-    #if(acao):
+   # if(acao):
 
-    if(acao == 5):
-        print("----------------------------------------------")
-        print("Bem vindo ao cadastro de uma nova reclamação")
-        nome = input("Informe seu nome: ")
-        tipo = input("Informe o tipo da reclamação --> Opções: |Elogio|Reclamação|Sugestão| ")
-        if(tipo.lower() != "elogio" and tipo.lower() != "reclamação" and tipo.lower() != "sugestão" ):
-            print("-----------------------------------------------------------------")
-            print("|       ! Tipo informado é invalido, tente novamente !          |")
-            print("-----------------------------------------------------------------")
-            continue
-        conteudo = input("Nos detalhe o que deseja manifestar: ")
-        novaReclamacao = (id,"#",nome + "#" + tipo + "#" + conteudo)
-        manifestacoes.append(novaReclamacao)
-        print("----------------------------------------------")
-        print("|   Nova manifestação criada com sucesso!    |")
-        continue
+   # if(acao):
 
-    #if(acao):
+ if (acao == 5):
+   cadastro()
+   # if(acao):
 
-    if(acao == 7):
-        iniciar = False
-        print("Obrigado por utilizar nosso sitema !")
-        print("----------------------------------------------")
-        continue
+ if (acao == 7):
+   sair()
+
+def cadastro():
+
+ global id
+ print("----------------------------------------------")
+ print("Bem vindo ao cadastro de uma nova reclamação")
+ nome = raw_input("Informe seu nome: ")
+ tipo = raw_input("Informe o tipo da reclamação ----> Opções: |Elogio|Reclamação|Sugestão| ")
+ if (tipo.lower() != "elogio" and tipo.lower() != "reclamação" and tipo.lower() != "sugestão"):
+   print("-----------------------------------------------------------------")
+   print("|       ! Tipo informado é invalido, tente novamente !          |")
+   print("-----------------------------------------------------------------")
+   menu()
+ conteudo = raw_input("Nos detalhe o que deseja manifestar: ")
+ novaReclamacao = (id + "#" + nome + "#" + tipo + "#" + conteudo)
+ manifestacoes.append(novaReclamacao)
+ print("----------------------------------------------")
+ print("|   Nova manifestação criada com sucesso!    |")
+ id = str(len(manifestacoes))
+ menu()
+def sair():
+ print("Obrigado por utilizar nosso sitema !")
+ print("----------------------------------------------")
+ exit()
+def listManifestacoes():
+ print("ID|NOME|TIPO|DESCRIÇÃO")
+ for manifestacao in manifestacoes:
+   saida = manifestacao.split("#")
+   teste = "|"
+   teste = teste.join(saida)
+   print(teste)
+
+ menu()
+#def listSugestoes():
+#def listReclamacoes():
+#def listElogios():
+#def getById():
+
+menu()
+
 
